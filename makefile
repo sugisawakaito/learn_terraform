ba0b05bf9a431e9566c3e623c63396565bbcca13
sugisawakaito <kaito24262426@gmail.com>
2023-01-24 19:44:57 +0900

    feature: make subnet

--- makefile
+++ makefile
@@ -2,25 +2,25 @@ help: ## ヘルプを表示する
     @grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "\033[36m%-30s\033[0m %s\n", $$1, $$2}'
 
 init: ## terraform init を実行
-    @terraform init
+    terraform init
 
 validate: ## tf ファイルを検証する
-    @terraform validate && printf "\e[32m***** validated ******\e[m\n"
+    terraform validate && printf "\e[32m***** validated ******\e[m\n"
 
 plan: validate ## terraform plan を実行する
-    @terraform plan
+    terraform plan
 
 apply: validate ## terraform apply を実行する
-    @terraform apply --auto-approve
+    terraform apply --auto-approve
 
 show: ## terraform show を実行する
-    @terraform show
+    terraform show
 
 destroy: ## terraform destroy を実行する
-    @terraform destroy
+    terraform destroy
 
 refresh: ## terraform refresh を実行する
-    @terraform refresh:w
+    terraform refresh:w
 
-test: ## ローカル環境で circleci を実行する
-    @circleci build .circleci/config.yml
+# test: ## ローカル環境で circleci を実行する
+#     circleci build .circleci/config.yml
--- network.tf
+++ network.tf
@@ -16,3 +16,64 @@ resource "aws_vpc" "vpc" {
   }
 
 }
+
+
+# ------------------------------------------
+# Create a Subnet
+# ------------------------------------------
+
+resource "aws_subnet" "public_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.1.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-subnet-1a"
+    project = var.project
+    ENV     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1a" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-subnet-1a"
+    project = var.project
+    ENV     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_subnet" "public_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.3.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-public-subnet-1c"
+    project = var.project
+    ENV     = var.environment
+    Type    = "public"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1c" {
+  vpc_id                  = aws_vpc.vpc.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.4.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-subnet-1c"
+    project = var.project
+    ENV     = var.environment
+    Type    = "private"
+  }
+}
