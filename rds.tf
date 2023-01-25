d74d240b0ad0dc57b621e01c974e35a2d2428174
sugisawakaito <kaito24262426@gmail.com>
2023-01-25 12:07:54 +0900

    feature: make rds subnet group

--- rds.tf
+++ rds.tf
@@ -24,3 +24,20 @@ resource "aws_db_option_group" "mysql_standalone_option_group" {
   engine_name          = "mysql"
   major_engine_version = "8.0"
 }
+
+# ---------------------------------
+# RDS subnet group
+# ---------------------------------
+resource "aws_db_subnet_group" "mysql_standalone_subnet_group" {
+  name = "${var.project}-${var.environment}-mysql-standalone-subnet-group"
+  subnet_ids = [
+    aws_subnet.private_subnet_1a.id,
+    aws_subnet.private_subnet_1c.id,
+  ]
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-mysql-standalone-subnet-group"
+    Project = var.project
+    Env     = var.environment
+  }
+}
