ee1bfc84a47447942883735315029bb256399e64
sugisawakaito <kaito24262426@gmail.com>
2023-01-25 09:32:25 +0900

    feature: add aws route tabels

--- network.tf
+++ network.tf
@@ -82,22 +82,64 @@ resource "aws_subnet" "private_subnet_1c" {
 # Create Route table
 # --------------------------------------------
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
-    Name    = "${var.project}-${var.environment}-public-rt"   
+    Name    = "${var.project}-${var.environment}-public-rt"
     project = var.project
-    Env    = var.environment
+    Env     = var.environment
     Type    = "public"
   }
 }
 
 resource "aws_route_table_association" "public_rt_association_1a" {
-  subnet_id      = aws_subnet.public_subnet_1a.id
   route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1a.id
 }
 
 resource "aws_route_table_association" "public_rt_association_1c" {
-  subnet_id = aws_route_table.public_rt.id
-  route_table_id = aws_route_table.public_subnet_1c.id
+  route_table_id = aws_route_table.public_rt.id
+  subnet_id      = aws_subnet.public_subnet_1c.id
+}
+
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-private-rt"
+    project = var.project
+    Env     = var.environment
+    Type    = "private"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_association_1a" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1a.id
+}
+
+resource "aws_route_table_association" "private_rt_association_1c" {
+  route_table_id = aws_route_table.private_rt.id
+  subnet_id      = aws_subnet.private_subnet_1c.id
+}
+
+# ---------------------------------------------
+# Create Internet Gateway
+# --------------------------------------------
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-igw"
+    project = var.project
+    Env     = var.environment
+  }
+}
+
+resource "aws_route" "public_rt_igw_r" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
 }
