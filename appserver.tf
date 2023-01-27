6ae4ddf15fb30913c80a6ae1a0de05a8f392e978
sugisawakaito <kaito24262426@gmail.com>
2023-01-27 18:55:46 +0900

    change key pair

--- appserver.tf
+++ appserver.tf
@@ -11,3 +11,25 @@ resource "aws_key_pair" "keypair" {
     Project = var.project
   }
 }
+
+# -----------------------------------
+# EC2 instance
+# ---------------------------------
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id,
+  ]
+  key_name = aws_key_pair.keypair.key_name
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-server"
+    Env     = var.environment
+    Project = var.project
+    Type    = "app"
+  }
+}
+
