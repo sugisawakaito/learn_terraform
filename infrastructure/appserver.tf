a4ab20274c07b100b85334d24e0a2fb83b61c99c
sugisawakaito <kaito24262426@gmail.com>
2023-10-08 11:01:42 +0900

    refactor

---
+++ infrastructure/appserver.tf
@@ -0,0 +1,71 @@
+# -----------------------------------
+# key pair
+# ---------------------------------
+resource "aws_key_pair" "keypair" {
+  key_name   = "${var.project}-${var.environment}-keypair"
+  public_key = file("./src/tastylog-dev-keypair.pub")
+
+  tags = {
+    Name    = "${var.project}-${var.environment}-keypair"
+    Env     = var.environment
+    Project = var.project
+  }
+}
+
+# -----------------------------------
+# SSH Parameter Store
+# ---------------------------------
+
+resource "aws_ssm_parameter" "host" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_HOST"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.address
+}
+
+resource "aws_ssm_parameter" "port" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PORT"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.port
+}
+
+resource "aws_ssm_parameter" "database" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_DATABASE"
+  type  = "String"
+  value = aws_db_instance.mysql_standalone.name
+}
+
+resource "aws_ssm_parameter" "username" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_USERNAME"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.username
+}
+
+resource "aws_ssm_parameter" "password" {
+  name  = "/${var.project}/${var.environment}/app/MYSQL_PASSWORD"
+  type  = "SecureString"
+  value = aws_db_instance.mysql_standalone.password
+}
+
+
+# -----------------------------------
+# EC2 instance
+# ---------------------------------
+resource "aws_instance" "app_server" {
+  ami                         = data.aws_ami.app.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnet_1a.id
+  associate_public_ip_address = true
+  iam_instance_profile        = aws_iam_instance_profile.app_ec2_profile.name
+  vpc_security_group_ids = [
+    aws_security_group.app_sg.id,
+    aws_security_group.opmng_sg.id,
+  ]
+  key_name = aws_key_pair.keypair.key_name
+  tags = {
+    Name    = "${var.project}-${var.environment}-app-server"
+    Env     = var.environment
+    Project = var.project
+    Type    = "app"
+  }
+}
+
